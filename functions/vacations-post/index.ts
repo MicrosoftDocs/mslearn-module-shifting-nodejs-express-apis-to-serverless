3ef308bfbc93d1b00358b13509d04ef78bc24ed4
John Papa <john@johnpapa.net>
2020-04-06 22:25:11 -0400

    fix delete

--- functions/vacations-post/index.ts
+++ functions/vacations-post/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { vacationService } from '../services';
 
-const httpTrigger: AzureFunction = async function(
+const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
