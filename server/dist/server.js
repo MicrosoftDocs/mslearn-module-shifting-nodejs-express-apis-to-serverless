6995940330e42383b8c687ca70541143286f7abd
John Papa <johnpapa@users.noreply.github.com>
2020-09-15 18:14:16 -0400

    Fix/audit sol (#10)
    * npm audit fix
    
    * ng update angular
    
    * ng up ngrx
    
    * ng up rxjs
    
    * npm up
    
    * 7071 is for functions
    
    * updating for func 1.2.2

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.start = void 0;
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const routes_1 = require("./routes");
