399be09dbb04c4b3b3fe062dc68067f6a835279b
John Papa <john@johnpapa.net>
2020-09-15 17:27:51 -0400

    updated node 12, angular 10, and npm update

--- server/dist/services/data.js
+++ server/dist/services/data.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getVacations = exports.deleteVacation = exports.updateVacation = exports.addVacation = void 0;
 const data = {
     vacations: [
         {
