27145f388de3dbe37acc3f786bb8947f9ca684ae
John Papa <john@johnpapa.net>
2020-09-15 17:57:34 -0400

    updating for func 1.2.2

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.router = router;
