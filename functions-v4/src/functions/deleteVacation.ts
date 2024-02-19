40e5da5ea960b3546d90d12cace9c4a9a47b8ce3
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-19 02:06:15 +0000

    chore: add `deleteVacation` function with error handling

--- functions-v4/src/functions/deleteVacation.ts
+++ functions-v4/src/functions/deleteVacation.ts
@@ -1,11 +1,41 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import * as vacationService from '../services/vacation.services';
 
 export async function deleteVacation(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    return { body: `Hello, ${name}!` };
+  context.log(`Http function processed request for url "${request.url}"`);
+  
+  try {
+    
+    const id = request.params.id;
+    
+    const deletedVacation = vacationService.deleteVacation(id);
+    
+    if (deletedVacation) {
+      return {
+        status: 204,
+        jsonBody: {
+          deleteVacation
+        }
+      };
+    } else {
+      return {
+        status: 404,
+        jsonBody: {
+          error: `Vacation with ID ${id} not found`
+        }
+      };
+    }
+  } catch (error: unknown) {
+    const err = error as Error;
+    context.error(`Error deleting vacation: ${err.message}`);
+    
+    return {
+      status: 500,
+      jsonBody: {
+        error: 'Failed to delete vacation'
+      }
+    };
+  }
 };
 
 
