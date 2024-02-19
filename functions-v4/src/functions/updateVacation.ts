debe0fd5ad0635af88ad7cc7a78c936928df915f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-19 02:01:01 +0000

    chore: add `updateVacation` function to handle vacation updates

--- functions-v4/src/functions/updateVacation.ts
+++ functions-v4/src/functions/updateVacation.ts
@@ -1,9 +1,39 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import * as vacationService from '../services/vacation.services';
+import { Vacation } from '../../models/vacation.models';
+
 
 export async function updateVacation(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    return { body: `Hello, ${name}!` };
+  try {
+    const id = request.params.id;
+    const { name, description } = await request.json() as Vacation;
+    
+    const updatedVacation = vacationService.updateVacation({ id, name, description });
+    
+    if (updatedVacation !== undefined) {
+      return {
+        status: 200,
+        jsonBody: {
+          updatedVacation
+        }
+      };
+    } else {
+      return {
+        status: 404,
+        jsonBody: {
+          error: `Vacation with ID ${id} not found`
+        }
+      };
+    }
+  } catch (error: unknown) {
+    const err = error as Error;
+    context.error(`Error updating vacation: ${err.message}`);
+    
+    return {
+      status: 500,
+      jsonBody: {
+        error: 'Failed to update vacation'
+      }
+    };
+  }
 };
