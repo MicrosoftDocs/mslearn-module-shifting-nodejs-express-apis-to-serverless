5151d448e0c3903d87325d5f009b71cd6cc9194c
Dina Berry (MSFT) <diberry@microsoft.com>
2024-02-21 22:31:50 +0000

    fixes for functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,28 +4,28 @@ import { postVacation } from './functions/postVacation';
 import { updateVacation } from './functions/updateVacation';
 import { deleteVacation } from './functions/deleteVacation';
 
-app.http('get-vacations', {
+app.http('getVacations', {
     methods: ['GET'],
     route: 'vacations',
     authLevel: 'anonymous',
     handler: getVacations
 });
 
-app.http('post-vacation', {
+app.http('postVacation', {
     methods: ['POST'],
     route: 'vacations',
     authLevel: 'anonymous',
     handler: postVacation
 });
 
-app.http('update-vacation', {
+app.http('updateVacation', {
     methods: ['PUT'],
     route: 'vacations/{id}',
     authLevel: 'anonymous',
     handler: updateVacation
 });
 
-app.http('delete-vacation', {
+app.http('deleteVacation', {
     methods: ['DELETE'],
     route: 'vacations/{id}',
     authLevel: 'anonymous',
